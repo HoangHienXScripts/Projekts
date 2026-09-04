187dcd37e457e1a52b759088b2de9c1eb72d9f87
Aci <hoangn1626@gmail.com>
2026-09-04 15:10:08 +0700

    Implement expiration check and update kick message
    Added expiration handling and player kick message.

--- TSB/fix_lag.lua
+++ TSB/fix_lag.lua
@@ -4,12 +4,17 @@ ws = game:GetService("Workspace")
 plrs = game:GetService("Players")
 
 local vars, plr
-vars = {}
+vars = {
+  expire = game:HttpGet("https://raw.githubusercontent.com/HoangHienXScripts/Projekts/refs/heads/main/TSB/time")
+}
 plr = plrs.LocalPlayer
 
-repeat task.wait()
-until game:IsLoaded()
-task.wait(2) plr:Kick("Chờ Update...")
+function _ft(s)
+	local d = math.floor(s / 86400)
+	local h = math.floor((s % 86400) / 3600)
+	local m = math.floor((s % 3600) / 60)
+	s = math.floor(s % 60) return string.format("%02d:%02d:%02d:%02d", d, h, m, s)
+end
 
 function find_plr(n)
   for _, v in pairs(plrs:GetPlayers()) do
@@ -18,4 +23,9 @@ function find_plr(n)
       return v
     end
   end
-end -- Hết thời gian, thứ này để sau --
+end
+
+repeat task.wait()
+until game:IsLoaded() and vars.expire and type(vars.expire) == "string"
+task.wait(2) plr:Kick("Updating, plz waitt... \nTime left: ".._ft(tonumber(vars.expire)))
+-- Hết thời gian, thứ này để sau --
