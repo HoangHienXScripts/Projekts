3206703494b92dbffa0a1c6521b9a6e5f15e1c0a
Aci <hoangn1626@gmail.com>
2026-09-04 14:55:32 +0700

    Enhance player search functionality
    Updated player finding logic to include DisplayName check.

--- TSB/fix_lag.lua
+++ TSB/fix_lag.lua
@@ -1,3 +1,4 @@
+-- Làm lại từ đầu
 local ws, plrs
 ws = game:GetService("Workspace")
 plrs = game:GetService("Players")
@@ -6,19 +7,15 @@ local vars, plr
 vars = {}
 plr = plrs.LocalPlayer
 
+repeat task.wait()
+until game:IsLoaded()
+task.wait(2) plr:Kick("Chờ Update...")
+
 function find_plr(n)
   for _, v in pairs(plrs:GetPlayers()) do
-    if v ~= plr and v.Name:lower():sub(1, #n) == n then
-      print(v.Name)
+    if v ~= plr and v.Name:lower():sub(1, #n) == n
+    or v.DisplayName:lower():sub(1, #n) == n then
+      return v
     end
   end
-end
-
-plr.Chatted:Connect(function(str)
-  str = str:split(" ")
-  if str[1] == "-find" then
-    if str[2] then
-      find_plr(str[2])
-    end
-  end
-end)
+end -- Hết thời gian, thứ này để sau --
