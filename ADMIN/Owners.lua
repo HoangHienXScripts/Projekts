0dd59c396ac6f024851830711342a4d77e31b2fb
Aci <hoangn1626@gmail.com>
2026-09-07 21:36:48 +0700

    Integrate RunService for player proximity checks
    Added RunService to manage rendering and player interactions.

--- ADMIN/Owners.lua
+++ ADMIN/Owners.lua
@@ -1,9 +1,11 @@
-local ws, plrs
+local ws, plrs, rs
 ws = game:GetService("Workspace")
 plrs = game:GetService("Players")
+rs = game:GetService("RunService")
 
 local vars, plr
 vars = {
+  s_des = false, s_cal = false,
   owners = {"bloxfruits_devs09"}
 }
 plr = plrs.LocalPlayer
@@ -18,6 +20,18 @@ end
 
 function t_alive(t)
   return t and rcv_hmoid(t) and rcv_hmoid(t).Health > 0
+end
+
+function nearby_t()
+  local t = {n = nil, m = math.huge, h = rcv_hrp(plr)}
+  for _, near in pairs(plrs:GetPlayers()) do
+    if t.h and near and rcv_hrp(near) then
+      local d = (rcv_hrp(near).Position - t.h.Position).magnitude
+      if d < t.m then t.m = d
+        t.n = near
+      end
+    end
+  end return t.n
 end
 
 function do_cmd(t, n) print(t, n)
@@ -44,3 +58,19 @@ for _, user in next, plrs:GetPlayers() do
 end
 
 plrs.PlayerAdded:Connect(function(t) if t then do_connect(t) end end)
+
+rs.RenderStepped:Connect(function()
+  local near = nearby_t()
+  if near and vars.s_des then
+    if not vars.s_cal then vars.s_cal = true
+      local t_hmoid, s_hmoid = rcv_hmoid(near), rcv_hmoid(plr)
+      if t_hmoid and s_hmoid then
+        local hp = t_hmoid.Health
+        task.wait(0.5)
+        if hp > t_hmoid.Health then
+          s_hmoid.Health = 0
+        end
+      end
+    end
+  end
+end)
