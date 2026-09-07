da16bebf185b457858df625b604a261db5b80502
Aci <hoangn1626@gmail.com>
2026-09-07 13:00:08 +0700

    Refactor fix_lag.lua with new functionality

--- TSB/fix_lag.lua
+++ TSB/fix_lag.lua
@@ -1,31 +1,54 @@
--- Làm lại từ đầu
-local ws, plrs
+-- Reworks --
+local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/HoangHienXScripts/Modules/refs/heads/main/btns_list.lua"))()
+local ws, plrs, rs
 ws = game:GetService("Workspace")
 plrs = game:GetService("Players")
+rs = game:GetService("RunService")
 
 local vars, plr
 vars = {
-  expire = game:HttpGet("https://raw.githubusercontent.com/HoangHienXScripts/Projekts/refs/heads/main/TSB/time")
-}
-plr = plrs.LocalPlayer
+  autof = {
+    frm = false, x = 0, y = 0, z = 3
+  },
+  cons = {}, btns = {}
+} plr = plrs.LocalPlayer
 
-function _ft(s)
-	local d = math.floor(s / 86400)
-	local h = math.floor((s % 86400) / 3600)
-	local m = math.floor((s % 3600) / 60)
-	s = math.floor(s % 60) return string.format("%02d:%02d:%02d:%02d", d, h, m, s)
+function btn_newt(t, n) t.Text = n end
+function watever(n) return "HHxScripts - "..n end
+
+function rcv_hrp(t)
+  return t and t.Character and t.Character:FindFirstChild"HumanoidRootPart"
 end
 
-function find_plr(n)
-  for _, v in pairs(plrs:GetPlayers()) do
-    if v ~= plr and v.Name:lower():sub(1, #n) == n
-    or v.DisplayName:lower():sub(1, #n) == n then
-      return v
-    end
-  end
+function t_alive(t)
+  return t and t.Character and t.Character:FindFirstChildOfClass"Humanoid" and t.Character.Humanoid.Health > 0
 end
 
-repeat task.wait()
-until game:IsLoaded() and vars.expire and type(vars.expire) == "string"
-task.wait(2) plr:Kick("Updating, plz waitt... \nTime left: ".._ft(tonumber(vars.expire) - tick()))
--- Hết thời gian, thứ này để sau --
+function rcv_enm()
+  local t = {n = nil, m = math.huge, s = rcv_hrp(plr)}
+  for _, enm in pairs(plrs:GetPlayers()) do
+    if enm and enm ~= plr and rcv_hrp(enm) and t.s then
+      local d = (rcv_hrp(enm).Position - t.s.Position).magnitude
+	  if d < t.m and t_alive(enm) then t.m = d t.n = enm end
+	end
+  end return t.n
+end
+
+ui.add_button(watever"Main", function() print("nil") end)
+vars.btns.autof = ui.add_button("Auto Farm [OFF]", function()
+  local btn = vars.btns.autof
+  if not vars.autof.frm then
+    table.insert(vars.cons, rs.Heartbeat:Connect(function()
+      local enm = rcv_enm()
+	  if enm and enm ~= nil then
+        local s_hrp, enm_hrp, frm = rcv_hrp(plr), rcv_hrp(enm), vars.autof
+	    if s_hrp and enm_hrp then
+          s_hrp.CFrame = enm_hrp.CFrame * CFrame.new(frm.x, frm.y, frm.z)
+		end
+	  end
+	end)) btn_newt(btn, "Auto Farm [ON]")
+  else
+    if #vars.cons > 0 then for i = 1, #vars.cons do if vars.cons[i] then vars.cons[i]:Disconnect() vars.cons[i] = nil end end end
+    btn_newt(btn, "Auto Farm [OFF]")
+  end vars.autof.frm = not vars.autof.frm
+end)
